0b370a253b8d723dc2ac2525c88981cf11969f0f
venkateswararaok@ismart.today <venkateswararaok@ismart.today>
2025-11-19 23:34:46 +0530

    sm updated

--- client/src/components/home/HeroBanner.tsx
+++ client/src/components/home/HeroBanner.tsx
@@ -516,7 +516,7 @@ const HeroBanner = () => {
                 src={video.src}
                 className="
                   w-full
-                  h-[155px] sm:h-[280px] md:h-[300px] lg:h-[375px]  /* 🔥 HEIGHT REDUCED 50% */
+                  h-[225px] sm:h-[380px] md:h-[300px] lg:h-[375px]  /* 🔥 HEIGHT REDUCED 50% */
                   object-cover rounded-xl
                 "
                 autoPlay
